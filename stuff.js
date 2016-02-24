b89c8c7fc0b94126f171f35e418c845a693005a8
ninabelikova <daimnevodka@gmail.com>
2016-02-25 00:17:40 +0800

    finish banner and intro

---
+++ stuff.js
@@ -0,0 +1,32 @@
+var app = angular.module('app', []);
+
+
+app.controller("PotatoControl", function ($scope, $timeout) {
+  $scope.oui = false;
+  $scope.nan = false;
+  $scope.potatoFondness = false;
+  $scope.potatoHatred = false;
+
+  $scope.yesTada = function () {
+    $scope.oui = true;
+    $scope.potatoFondness = true;
+    $scope.potatoHatred = false;
+    $timeout($scope.cancelYesTada, 1000);
+  };
+
+  $scope.noTada = function () {
+    $scope.nan = true;
+    $scope.potatoHatred = true;
+    $scope.potatoFondness = false;
+    $timeout($scope.cancelNoTada, 1000);
+  };
+
+  $scope.cancelYesTada = function () {
+    $scope.oui = false;
+  };
+
+  $scope.cancelNoTada = function () {
+    $scope.nan = false;
+  }
+
+});
